790868ef93471e9905b4f8df9048732094193cc3
eprogramming <00hf11@gmail.com>
2015-04-12 11:36:49 -0300

    exception

---
+++ exception.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for f in $(find . -type f -name '*.groovy'); do
+  mv $f $f.tmp
+  sed 's/catch(Exception e)/catch (Exception e)/g' $f.tmp > $f
+  rm -f $f.tmp
+done
+
+for f in $(find . -type f -name '*.groovy'); do
+  mv $f $f.tmp
+  sed 's/catch (Exception e)/catch (e)/g' $f.tmp > $f
+  rm -f $f.tmp
+done
