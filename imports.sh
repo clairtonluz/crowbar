6281e468dc8fb83da4d6436d08e1e92de5ab1995
eprogramming <00hf11@gmail.com>
2015-04-12 15:41:42 -0300

    imports

--- imports.sh
+++ imports.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# removes from all groovy files the java.io.* import
-
 for f in $(find . -type f -name '*.groovy'); do
   mv $f $f.tmp
   sed 's/import java.io.*//g' $f.tmp > $f
   rm -f $f.tmp
+
+  mv $f $f.tmp
+  sed 's/import java.util.*//g' $f.tmp > $f
+  rm -f $f.tmp
 done
+
+
+
--- util.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# removes all import java.util.* from groovy files
-
-for f in $(find . -type f -name '*.groovy'); do
-  mv $f $f.tmp
-  sed 's/import java.util.*//g' $f.tmp > $f
-  rm -f $f.tmp
-done
